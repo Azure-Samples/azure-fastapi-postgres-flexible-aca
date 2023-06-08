d77554c32403b4e2434a142b48f24f03d4b0cde8
Jay Miller <kjaymiller@gmail.com>
2023-06-08 15:29:27 -0700

    Replace DjangoORM with SQL Model Fixes #1

--- demo-code/app.py
+++ demo-code/app.py
@@ -1,6 +1,48 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+import models
 
 app = FastAPI()
+app.mount('/mount', StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-if __name__ == "__main__":
-    app.run()
+@app.on_event("startup")
+def on_startup():
+    models.create_db_and_tables()
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# @app.get("/about")
+# def about(request):
+#     return render(request, "about.html")
+
+# @app.get("/destinations")
+# def destinations(destination=Destination):
+#     all_destinations = models.Destination.objects.all()
+#     return render(request, "destinations.html", {"destinations": all_destinations})
+
+
+# @app.get("/destination/{pk}")
+# class DestinationDetailView(generic.DetailView):
+#     template_name = "destination_detail.html"
+#     model = models.Destination
+#     context_object_name = "destination"
+
+# @app.get("/cruise/{pk}")
+# class CruiseDetailView(generic.DetailView):
+#     template_name = "cruise_detail.html"
+#     model = models.Cruise
+#     context_object_name = "cruise"
+
+# @app.get("/info_request")
+# class InfoRequestCreate(SuccessMessageMixin, generic.CreateView):
+#     template_name = "info_request_create.html"
+#     model = models.InfoRequest
+#     fields = ["name", "email", "cruise", "notes"]
+#     success_url = reverse_lazy("index")
+#     success_message = "Thank you, %(name)s! We will email you when we have more information about %(cruise)s!"
---
+++ demo-code/models.py
@@ -0,0 +1,53 @@
+# ruff: noqa: UP007
+# ruff: noqa: UP006
+
+import typing
+
+from sqlmodel import Field, Relationship, SQLModel, create_engine
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+# Create your models here.
+class CruiseDestinationLink(SQLModel, table=True):
+    destination_id: typing.Optional[int] = Field(
+        default=None,
+        foreign_key="destination.id",
+        primary_key=True,
+    )
+    cruise_id: typing.Optional[int] = Field(
+        default=None,
+        foreign_key="cruise.id",
+        primary_key=True,
+    )
+
+
+class Destination(SQLModel, table=True):
+    id: typing.Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    description: typing.Optional[str]
+
+
+class Cruise(SQLModel, table=True):
+    id: typing.Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    description: typing.Optional[str]
+    destinations: typing.List["Destination"] = Relationship(
+        back_populates="destinations",
+        link_model=CruiseDestinationLink,
+    )
+
+
+class InfoRequest(SQLModel, table=True):
+    id: typing.Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    email: str
+    notes: str
+    cruise_id: int = Field (default=None, foreign_key="cruise.id")
