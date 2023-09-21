dc39555d3d91a185566a1c2be25addf56c012df4
Pamela Fox <pamela.fox@gmail.com>
2023-09-21 20:29:31 +0000

    Update from cruft

---
+++ src/fastapi_app/app.py
@@ -0,0 +1,62 @@
+import os
+import pathlib
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from sqlmodel import Session, select
+
+from .models import Cruise, Destination, InfoRequest, engine
+
+app = FastAPI()
+parent_path = pathlib.Path(__file__).parent.parent
+app.mount("/mount", StaticFiles(directory=parent_path / "static"), name="static")
+templates = Jinja2Templates(directory=parent_path / "templates")
+templates.env.globals["prod"] = os.environ.get("RUNNING_IN_PRODUCTION", False)
+
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/about", response_class=HTMLResponse)
+def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
+
+
+@app.get("/destinations", response_class=HTMLResponse)
+def destinations(request: Request):
+    with Session(engine) as session:
+        all_destinations = session.exec(select(Destination)).all()
+    return templates.TemplateResponse("destinations.html", {"request": request, "destinations": all_destinations})
+
+
+@app.get("/destination/{pk}", response_class=HTMLResponse)
+def destination_detail(request: Request, pk: int):
+    with Session(engine) as session:
+        destination = session.exec(select(Destination).where(Destination.id == pk)).first()
+        return templates.TemplateResponse("destination_detail.html", {"request": request, "destination": destination})
+
+
+@app.get("/cruise/{pk}")
+def cruise_detail(request: Request, pk: int):
+    with Session(engine) as session:
+        cruise = session.exec(select(Cruise).where(Cruise.id == pk)).first()
+        return templates.TemplateResponse("cruise_detail.html", {"request": request, "cruise": cruise})
+
+
+@app.get("/info_request/", response_class=HTMLResponse)
+def info_request(request: Request):
+    return templates.TemplateResponse("info_request_create.html", {"request": request})
+
+
+@app.post("/info_request/", response_model=InfoRequest)
+def create_info_request(info_request: InfoRequest):
+    with Session(engine) as session:
+        db_info_request = InfoRequest.from_orm(info_request)
+        session.add(db_info_request)
+        session.commit()
+        session.refresh(db_info_request)
+        return db_info_request
---
+++ src/fastapi_app/models.py
@@ -0,0 +1,71 @@
+# ruff: noqa: UP007
+# ruff: noqa: UP006
+import os
+import typing
+
+from sqlmodel import Field, Relationship, SQLModel, create_engine
+
+POSTGRES_USERNAME = os.environ.get("POSTGRES_USERNAME")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+POSTGRES_DATABASE = os.environ.get("POSTGRES_DATABASE")
+
+sql_url = f"postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DATABASE}"
+
+if os.environ.get("POSTGRES_SSL", "disable") != "disable":
+    sql_url = f"{sql_url}?sslmode=require"
+
+engine = create_engine(sql_url, echo=True)
+
+
+def create_db_and_tables():
+    return SQLModel.metadata.create_all(engine)
+
+
+class CruiseDestinationLink(SQLModel, table=True):
+    destination_id: typing.Optional[int] = Field(
+        default=None,
+        foreign_key="destination.id",
+        primary_key=True,
+    )
+    cruise_id: typing.Optional[int] = Field(
+        default=None,
+        foreign_key="cruise.id",
+        primary_key=True,
+    )
+
+
+class Destination(SQLModel, table=True):
+    id: typing.Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    subtitle: typing.Optional[str]
+    description: typing.Optional[str]
+    cruises: typing.List["Cruise"] = Relationship(
+        back_populates="destinations",
+        link_model=CruiseDestinationLink,
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Cruise(SQLModel, table=True):
+    id: typing.Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    description: typing.Optional[str]
+    subtitle: typing.Optional[str]
+    destinations: typing.List["Destination"] = Relationship(
+        back_populates="cruises",
+        link_model=CruiseDestinationLink,
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class InfoRequest(SQLModel, table=True):
+    id: typing.Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    email: str
+    notes: str
+    cruise_id: int = Field(default=None, foreign_key="cruise.id")
