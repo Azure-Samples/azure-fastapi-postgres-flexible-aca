aa6b1ac6c94df72705d674e6e1bbfa98afbcaaa7
Jay Miller <kjaymiller@gmail.com>
2024-01-17 12:27:14 -0500

    Cruft Update

--- src/fastapi_app/__init__.py
+++ src/fastapi_app/__init__.py
@@ -1,1 +1,9 @@
+import os
+
+from azure.monitor.opentelemetry import configure_azure_monitor
+
 from .app import app  # noqa
+
+if os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"):
+    configure_azure_monitor()
+__all__ = ["app"]
